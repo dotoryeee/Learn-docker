dad58033836905a417ba25161d19c75b0a0df1b4
dotoryeee <bayer@kakao.com>
2021-03-06 10:50:26 +0900

    compose fix

--- compose/server.js
+++ compose/server.js
@@ -8,13 +8,14 @@ const REDIS_PORT = 6379;
 const client = redis.createClient({
     //host값에 도커가 아닌 일반 환경에서는 https://redis-server.com 을 입력
     //compose를 사용할 경우 docker-compose.yml에 명시한 컨테이너 이름을 입력
-    host: "redis-server";  
-    port: REDIS_PORT;
+    host: "redis-server",
+    port: REDIS_PORT
 })
 
 client.set("number", 0);
 
 //express
+const app = express();
 app.get('/', (req, res) => {
     client.get("number", (err, Number) => { //에러가 발생하지 않는다면
         //숫자를 1씩 증가
@@ -23,6 +24,5 @@ app.get('/', (req, res) => {
     })
 })
 
-const app = express();
 app.listen(EXPRESS_PORT);
 console.log('server is running');
